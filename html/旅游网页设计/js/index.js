1bb0981e79ebf464ff8adf3cb0c51f6bb815612b
feng <289468343@qq.com>
2015-10-01 21:37:38 +0800

    利用JQUERY,更新了分类具体内容的特效

---
+++ html/旅游网页设计/js/index.js
@@ -0,0 +1,108 @@
+/*纯JS实现的功能写入此处*/
+
+/*滑动图片背景渐变深化颜色,加style
+*   1.this的用法很关键
+*   2.理解for循环结构实质，最后生成的文档为4段代码，无变量的，
+*       所以触发for循环里的事件onmouseover时，不会执行事件onmouseover前边的代码var bl=true
+*/
+    var img_box =  document.getElementsByClassName('sp-box');
+    for(var i=0;i<img_box.length;i++){
+        var bl=true,is_this;
+        /*鼠标移动到图上时*/
+        img_box[i].onmouseover=function(){
+            if(is_this!=this)bl=true;
+            is_this=this;//区分是否是本块，减少因速度太快，没有触发清除定时器的错误率
+            if(bl==false)return;
+            bl=false;
+            var xs= 70,s_time;//默认像素高度
+            var hs_id=this.getElementsByClassName('img_shadow');
+            //clearTimeout(s_time);
+            s_time=setInterval(function(){
+                if(xs <= 0){
+                    clearTimeout(s_time);
+                    bl=true;
+                    return;
+                }
+                xs-=10;
+                hs_id[0].style.top=xs+"px";
+            },1);
+        }
+        /*鼠标移开时*/
+        img_box[i].onmouseout =function(){
+            if(is_this!=this)bl=true; is_this=this;
+            if(bl==false)return;bl=false;
+            var xs=0,time;
+            var hs_id=this.getElementsByClassName('img_shadow');
+            //clearTimeout(time);
+            time=setInterval(function(){
+                if(xs>=70){
+                    hs_id[0].setAttribute('style','');
+                    clearTimeout(time);
+                    bl=true;
+                    return;
+                }
+                xs+=10;
+                hs_id[0].style.top=xs+"px";
+            },1)
+
+            //img_shadow[3].setAttribute('style','')
+        }
+    }
+
+
+/*点击文字，大广告图切换，加class*/
+
+
+    var slide_img = document.getElementsByClassName('ad_pic');
+    var word_box = document.getElementsByClassName('word_box');
+    for(var i=0;i<word_box.length;i++){
+        word_box[i]=this;//做一个记录，解决下标问题
+        word_box[i].onmouseover=function(){
+            for(var j=0;j<word_box.length;j++){
+                move_class('ad_show',slide_img[j]);
+                if(word_box[j]==this) k=j}//如果相等了，记录下标，后边用
+            add_class('ad_show',slide_img[k]);
+            clearInterval(time_roll);
+        }
+    }
+
+    function auto_roll(x){
+        for(var j=0;j<word_box.length;j++){move_class('ad_show',slide_img[j]);}
+        add_class('ad_show',slide_img[x]);
+    }
+    var y=0;
+    var time_roll = setInterval(function(){
+        auto_roll(y);
+        y+=1;
+        if(y==word_box.length)y=0;
+    },2500);
+    /**
+     *  添加class
+     *  @param ad_cls:要添加的class名字
+     *  @param cls_name 所要操作的DOM节点，已经有document.getElementByClassName获取
+     */
+    function add_class(ad_cls,cls_name){
+            var temp = cls_name.getAttribute('class');
+            temp +=' '+ad_cls;
+            cls_name.setAttribute('class',temp);
+         }
+
+    /**
+     *  删除class
+     *  @param del_cls:要删除的class名字
+     *  @param cls_name 所要操作的DOM节点，已经有document.getElementByClassName获取
+     */
+    function move_class(del_cls,cls_name){
+            var temp = cls_name.getAttribute('class');
+            if(!temp)return;
+            temp = temp.replace(' '+del_cls,'');
+            cls_name.setAttribute('class',temp)
+        }
+
+
+/*设置侧面浮动导航*/
+    $(function(){
+        var ver_menu=$('.ver_menu');
+        var showlist=$('.showlist');
+        //alert(showlist.innerWidth())
+    });
---
+++ html/旅游网页设计/js/jq.js
@@ -0,0 +1,176 @@
+/**
+ * Created by hanfeng on 2015/9/30.
+ */
+/*把Jquery代码写入此中*/
+$(function(){
+    var main =$('#big_show');
+    var left_menu=$('#left_menu');
+    var lf_list=$('#left_menu li');
+    var main_list=$('#big_show .showlist');
+
+    ct_distance();
+    /**
+     * 计算左边导航的漂浮距离，紧贴main左侧
+     */
+    function ct_distance(){
+        var win_width=parseFloat($(window).width());
+        var contain_width=parseFloat(main.width());
+        var menu_width=parseFloat(left_menu.width());
+        left_dis=(win_width-contain_width)/2-menu_width-1;
+        left_menu.css('left',left_dis);
+    }
+    //设置顶部隐藏
+    function hidden_menu(){
+        if($(window).scrollTop()<200){
+            left_menu.hide();
+        }else{
+            left_menu.show();
+        }
+    }
+    hidden_menu();
+
+
+    /**
+     * 设置点击左边导航实现main滚动到对应模块
+     */
+    lf_list.click(function(){
+        lf_list.removeClass('lt-hover');//1.移除CLASS
+        $(this).addClass('lt-hover');//2.添加CLASS
+        var key = $(this).index();//获取当前点击的下标，为了获取与MAIN对应的模块
+        var showlist = main_list.eq(key);//3.获取到点的对象对应main中的目标是哪块
+        var dis= showlist.offset().top-110;//目标main模块的滚动条对应的高度
+        var cur= $(window).scrollTop();//目标main模块的滚动条对应的高度
+        roll_fn(cur,dis,function(i){
+            $(window).scrollTop(i);
+        },20,100)
+    });
+
+    /**
+     * 设main滚动，联动左边导航
+     */
+    $(window).scroll( function() {
+        var now_scroll=$(window).scrollTop(),key;
+        $.each(main_list,function(i,v){
+           var main_list_dis =  main_list.eq(i).offset().top;
+            if(now_scroll>=main_list_dis-502) key=i;
+        });
+        lf_list.removeClass('lt-hover');
+        lf_list.eq(key).addClass('lt-hover');
+        //设置顶部隐藏
+        if($(window).scrollTop()<200){
+            left_menu.hide();
+        }else{
+            left_menu.show();
+        }
+        hidden_menu();
+    } );
+
+
+
+    /*设置黑色广告的关闭按钮旋转*/
+    var ad_box=$('#ad_box');
+    var btn=$('#ad_box .ad_close');
+    btn.css('margin-left',left_dis+55);
+    btn.click(function(){
+        ad_box.animate({width:0,opacity:0},300)
+    });
+    //设置旋转
+    btn.hover(
+        function(){
+            roll_fn(0,180,function(i){
+                btn.css('transform','rotate('+i+'deg)')
+            },10,10)
+        },
+        function(){
+            roll_fn(180,0,function(i){
+                btn.css('transform','rotate('+i+'deg)')
+            },10,10)
+        }
+    );
+
+    /**
+     * 设置动画效果，触发器
+     * @param org 初始状态
+     * @param aim 目标状态
+     * @param fn 所执行的函数
+     * @param speed 执行速度
+     * @param speed 一次执行的尺度大小
+     */
+    function roll_fn(org,aim,fn,speed,size){
+        var ff=org;
+        var test = setInterval(function(){
+            if(org<aim){
+                if(ff>=aim){
+                    clearInterval(test);
+                    return;
+                }
+                ff+=size;
+            }else{
+                if(ff<=aim){
+                    clearInterval(test);
+                    return;
+                }
+                ff-=size;
+            }
+            fn(ff);
+        },speed)
+    }
+
+    /*中间旅游图效果*/
+
+    /*导航与内容同步切换*/
+    var showlist=$('.showlist');
+    $.each(showlist,function(){
+        var t_right=$(this).find('.t_right span');//获得当前大分类下的每一个导航
+        var detail_list=$(this).find('.content_detail .cd_right');//获得当前大分类下的小分类内容
+        t_right.hover(function(){
+                t_right.removeClass('tr-hover');
+                $(this).addClass('tr-hover');
+                var k=t_right.index($(this));//获取当前下标值
+                detail_list.removeClass('cr-hover');
+                detail_list.eq(k).addClass('cr-hover');
+            },
+            function(){});
+
+        /*鼠标滑动小图背景*/
+        var rl_img=detail_list.find('.rl_img');//获取该分类下的所有6个小图
+        rl_img.hover(function(){
+                var a=$(this).find('.rl_bg');
+                a.animate({top:-125},'fast');
+            },
+            function(){
+                var a=$(this).find('.rl_bg');
+                a.animate({top:125},'fast');
+            });
+    });
+
+
+});
+
+/**
+ * JQUERY总结
+ * 实现功能：
+ *      1.导航条点击滚动
+ *            原理：点击触发，导航条的个数与main的个数对应，【利用下标值对应相同】，结合滚动高度来设置
+ *            关键JQ方法：
+ *                  $(window).scrollTop()设置滚动条的距离
+ *                  obj.offset().top;获取当前对象距网页头部高度
+ *      2.滚动图片导航条滚动
+ *            原理：滚动触发，获取当前滚动高度，获取每一个块的高度，进行判断，【记录滚动到当前区域的下标】
+ *                  从而利用【下标对应关系】找到导航里的对应模块
+ *            关键JQ方法：
+ *                   $.each() 循环获得的CLASS，从而操作里边每一个DIV
+ *                   obj.offset().top;获取当前对象距网页头部高度
+ *
+ *      3.广告栏关闭按钮旋转
+ *            原理：     利用CSS3属性
+ *            关键JQ方法：transfor:rotate(10beg)
+ *
+ *      利用函数
+ *          roll_fn():利用定时器实现参数的慢慢变化过程
+ */
+
+
+
+
+
