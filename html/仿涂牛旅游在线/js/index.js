0a7298c6510fc5a2f10b23b86b0e02a5eda21f4f
feng <289468343@qq.com>
2015-10-03 08:58:15 +0800

    仿涂牛主页初始化

---
+++ html/仿涂牛旅游在线/js/index.js
@@ -0,0 +1,13 @@
+$(function(){
+    var top_ad=$('#top-ad');
+    //top_ad.show(1000);
+    top_ad.find('.ad-close').click(function(){
+        top_ad.hide(1000);
+    });
+
+
+
+
+
+
+});/*end $()*/
